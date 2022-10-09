bbb98aeac24516001edde30b6b4ea182a0c7a170
Karina Valens <karyvalens@yahoo.com>
2022-10-10 00:12:46 +0200

    fetch restdb.io data base retriving first assigment

--- js/script.js
+++ js/script.js
@@ -2,12 +2,34 @@
 document.addEventListener("DOMContentLoaded", init)
 
 const url = "https://karinavalens-35ec.restdb.io/rest/portafolio";
+const restioApiKey = {
+    headers: {
+        "x-apikey": "62838b6de8128861fcf3d3b6",
+    },
+};
 //api key:6c03100ae96764d9a39b7dc5c9425413673c3
 
 
 function init() {
+    
+    loadJson();
     setupBurger();
-    startAnim();
+    startHomePageAnim();
+}
+
+async function loadJson() {
+    const res = await fetch(url, restioApiKey);
+    const projectsData = await res.json();
+
+    // when loaded, handleProjects
+    handleProjects(projectsData);
+    console.log("projectsData", projectsData);
+}
+
+function handleProjects(projectsData) {
+    projectsData.forEach(displayProject);
+    console.log("projectsData", projectsData);
+
 }
 
 function setupBurger() {
@@ -21,24 +43,24 @@ function setupBurger() {
     });
 }
 
-const skillUX = document.querySelector("#ux_ui");
-const skillFE = document.querySelector("#front_end");
-const skillMD = document.querySelector("#mult_designer");
-
 /* --------- //SKILLS// --------- */
-
-
-
-function startAnim() {
-
-    const skillsKeyFrames = [{
+function startHomePageAnim() {
+    const skillUX = document.querySelector("#ux_ui");
+    const skillFE = document.querySelector("#front_end");
+    const skillMD = document.querySelector("#mult_designer");
+    const skillsKeyFrames = [
+        /* {
+                    transform: 'translate(0,15vw) scale(0, 0)',
+                    easing: 'ease-in'
+                }, */
+        {
             transform: 'translate(-40vw,15vw) scale(0, 0)',
             easing: 'ease-in'
         }, {
-            transform: 'translate(-20vw,20vw) scale(.6, .6)',
+            transform: 'translate(-20vw,20vw) scale(0, 0)',
         },
         {
-            transform: 'translate(-10vw,25vw) scale(.9, .9)',
+            transform: 'translate(-10vw,25vw) scale(.3, .3)',
         },
         {
             transform: 'translate(20vw,30vw) scale(1, 1)',
@@ -60,12 +82,6 @@ function startAnim() {
         iterations: Infinity,
     }
 
-    /* const skills = document.querySelectorAll(".skills");
-    skills.forEach(skill => {
-        skill.classList.add("visible");
-
-    }); */
-
     skillUX.animate(skillsKeyFrames, skillsProperties);
     setTimeout(() => {
         skillFE.animate(skillsKeyFrames, skillsProperties)
@@ -73,4 +89,30 @@ function startAnim() {
     setTimeout(() => {
         skillMD.animate(skillsKeyFrames, skillsProperties)
     }, 4000);
+}
+
+
+//hacer a search filter for projects
+/* ----- //RETRIVE JSON DATA// ---------- */
+
+function displayProject(project) {
+    console.log(project);
+    // 1.- Select the content from the template
+    const template = document.querySelector("#template").content;
+    // 2.- Clone the template
+    const clone = template.cloneNode(true);
+    // 3.- Populate the data
+    clone.querySelector(".figcap-projects-page").textContent = project.name;
+    clone.querySelector("img").src = project.img;
+    clone.querySelector("img").alt = project.name;
+    //on hover card
+    clone.querySelector("#project-name").textContent = project.name;
+    clone.querySelector("p").textContent = project.description;
+
+
+
+    // 4.- Select the new parent element in the dom
+    const parent = document.querySelector("#projects-list");
+    // 5.- Appen  the child to the new parent element inside the Dom
+    parent.appendChild(clone);
 }
